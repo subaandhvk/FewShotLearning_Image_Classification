6bdac2205e860256f6612dfcab424e292dc559be
subaandh-VK <subaandh@gmail.com>
2021-07-22 17:35:46 +0200

    Integrate frontend with models

--- app.py
+++ app.py
@@ -4,54 +4,21 @@ from PIL import Image
 from flask import Flask, request, render_template
 from keras.models import load_model
 import zipfile
-from notebooks.simple_fruit_detector_by_cnn import train_model
+import os
+import shutil
+from notebooks.cnn_transfer_learning import train_model, predict_cnn_model
 
 app = Flask(__name__)
 
 # Import the model
-class_names = ['apple', 'broccoli', 'grape', 'lemon']
+class_names = ['apple', 'broccoli', 'grape', 'lemon', 'orange', 'strawberry']
 CNN_model = load_model("CNN-output.h5")
 
 global results
 results = []
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-def predict_cnn(data):
-    print('Started function processing')
-    data = [data]
-    data = np.array(data)
-    print(data.shape)
-
-    predictions = CNN_model.predict(data)
-
-    print('Before 1', predictions, predictions.shape)
-
-    predictions = class_names[np.argmax(predictions)]
-
-    print(predictions)
-
-    return predictions
-
-
-def predict_cnn2(data):
-    print('Processing function 2')
-    data = [data]
-    data = np.array(data)
-    print(data.shape)
-
-    predictions = CNN_model.predict(data)
-    print('Before 2', predictions, predictions.shape)
-
-    predictions = class_names[np.argmax(predictions)]
-    print(predictions)
-
-    return predictions
-
 
 def get_result(result):
     results.append(result)
@@ -59,45 +26,65 @@ def get_result(result):
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.files['img_file']:
-        print(request.files.values())
-        img = Image.open(request.files['img_file'])
-        print('*****************************************************************')
+    pred1 = pred2 = pred3 = None
+    pool = mp.Pool(mp.cpu_count())
+    output = []
 
-        img = img.resize((400, 400))
+    if request.files['img_file']:
+        img = Image.open(request.files['img_file'])
+
+        img = img.resize((200, 200))
         img = [np.array(img)]
 
-        output = []
-        pool = mp.Pool(mp.cpu_count())
-        res1 = pool.apply_async(predict_cnn, args=(img), callback=get_result)
-        res2 = pool.apply_async(predict_cnn2, args=(img), callback=get_result)
-
+        res1 = pool.apply_async(predict_cnn_model, args=(img), callback=get_result)
         output.append(res1)
+
+    if request.files['img_file2'] and request.files['img_file']:
+        print('process2')
+        f1 = request.files['img_file']
+        f2 = request.files['img_file2']
+
+        path = './datasets/new'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        else:
+            shutil.rmtree(path)
+
+        f1.save('./datasets/new/0.jpg')
+        f2.save('./datasets/new/1.jpg')
+
+        res2 = pool.apply_async(predict_triplets, args=(), callback=get_result)
         output.append(res2)
-        pool.close()
-        pool.join()
 
-        [r.wait() for r in output]
+        pass
 
-        print(results)
-        if len(results) == 2:
-            pred1 = results[0]
-            pred2 = results[1]
-            # pred3 = results[2]
+    pool.close()
+    pool.join()
 
-        return render_template('index.html', prediction1=f" CNN Result : {pred1}",
-                               prediction2=f"Constrastive Loss Result: {pred2}", prediction3='Three')
+    [r.wait() for r in output]
+
+    if len(results) == 2:
+        pred1 = results[0]
+        pred2 = results[1]
+    elif len(results) == 1:
+        pred1 = results[0]
+
+    results.clear()
+    output.clear()
+
+    return render_template('index.html', prediction1=f" CNN Result : {pred1}",
+                            prediction2=f"Triplet Loss Result: {pred2}")
 
 
 @app.route('/train', methods=['POST'])
 def train():
     if request.files['data_file']:
         with zipfile.ZipFile(request.files['data_file'], "r") as zip_ref:
-            zip_ref.extractall('../datasets/.')
+            zip_ref.extractall('./datasets/.')
 
-        # model = train_model()
+        model = train_model()
 
-    return render_template('index.html', prediction1='Model training successful')
+    return render_template('train.html', result='Model training successful')
 
 
 if __name__ == '__main__':
---
+++ notebooks/cnn_transfer_learning.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import keras_preprocessing
+from keras_preprocessing import image
+from keras_preprocessing.image import ImageDataGenerator
+import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+from keras.models import load_model
+
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from joblib import dump,load
+
+
+def train_model():
+    directory = './datasets/'
+
+    train_dir = valid_dir = directory
+
+    train_datagen = ImageDataGenerator(rescale = 1./255,
+                                      rotation_range=40,
+                                      width_shift_range=0.2,
+                                      height_shift_range=0.2,
+                                      shear_range=0.2,
+                                      zoom_range=0.2,
+                                      horizontal_flip=True,
+                                      fill_mode='nearest',
+                                      validation_split=0.2)
+
+
+# valid_datagen = ImageDataGenerator(rescale = 1./255)
+
+    train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    target_size=(200,200),
+                                                    class_mode='categorical',
+                                                    batch_size=5,
+                                                    subset='training')
+
+    valid_generator = train_datagen.flow_from_directory(valid_dir,
+                                                    subset='validation',
+                                                    target_size=(200,200),
+                                                    class_mode='categorical',
+                                                    batch_size=5,
+                                                    )
+    
+    class_names = list(train_generator.class_indices)
+    num_classes = len(set(class_names))
+
+    vgg_model = VGG16(include_top=False, input_shape=(200, 200, 3))
+
+    for layer in vgg_model.layers:
+        layer.trainable = False
+
+    flat1 = Flatten()(vgg_model.layers[-1].output)
+    class1 = Dense(256, activation='relu')(flat1)
+    output = Dense(num_classes, activation='softmax')(class1)
+
+    model = Model(inputs = vgg_model.inputs, outputs = output)
+    
+    model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    history = model.fit(train_generator,
+                    validation_data = valid_generator,
+                    epochs=5, verbose=1)
+
+    # model.save("../models/CNN-output.h5")
+    out = []
+    res = []
+
+    for val in range(len(valid_generator)):
+        (a, b) = valid_generator[val]
+        out.append(a)
+        res.append(b)
+
+    print(len(out), len(res))
+    o = out[0]
+    r = res[0]
+
+    for i in range(1, len(out)):
+        o = np.concatenate((o, out[i]))
+        r = np.concatenate((r, res[i]))
+
+    pred = model.predict(o)
+    pred = [np.argmax(val) for val in pred]
+    r = [np.argmax(val) for val in r]
+    
+    cm = confusion_matrix(r, pred)
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    cmd = ConfusionMatrixDisplay(cm, display_labels=class_names)
+
+    cmd.plot()
+    cmd.ax_.set(title='Confusion Matrix',
+            xlabel='Predicted Fruits',
+            ylabel='Actual Fruits')
+
+    plt.savefig('./cnn.jpg')
+    
+    return model
+
+def predict_cnn_model(data):
+    directory = './datasets/'
+
+    train_dir = valid_dir = directory
+
+    train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                       rotation_range=40,
+                                       width_shift_range=0.2,
+                                       height_shift_range=0.2,
+                                       shear_range=0.2,
+                                       zoom_range=0.2,
+                                       horizontal_flip=True,
+                                       fill_mode='nearest',
+                                       validation_split=0.2)
+
+    # valid_datagen = ImageDataGenerator(rescale = 1./255)
+
+    train_generator = train_datagen.flow_from_directory(train_dir,
+                                                        target_size=(200, 200),
+                                                        class_mode='categorical',
+                                                        batch_size=5,
+                                                        subset='training')
+
+    class_names = list(train_generator.class_indices)
+    num_classes = len(set(class_names))
+
+    print(class_names, num_classes)
+    data = [data]
+    data = np.array(data)
+    print(data.shape)
+
+    model = load_model('./CNN-output.h5')
+    pred = model.predict(data)
+
+    print('predictions=', pred)
+    predictions = class_names[np.argmax(pred)]
+    print('final value', predictions)
+
+    return predictions
+
+
